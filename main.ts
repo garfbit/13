3e8bb17075abdd0ea6cd28002a960d9e1babd87f
garfbit <104676660+garfbit@users.noreply.github.com>
2022-09-11 07:00:34 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,19 @@
+let Caracter = ""
+basic.forever(function () {
+    Caracter = "I"
+    if (Caracter == "a" || Caracter == "A") {
+        basic.showString("ES VOCAL")
+    }
+    if (Caracter == "e" || Caracter == "E") {
+        basic.showString("ES VOCAL")
+    }
+    if (Caracter == "i" || Caracter == "I") {
+        basic.showString("ES VOCAL")
+    }
+    if (Caracter == "o" || Caracter == "O") {
+        basic.showString("ES VOCAL")
+    }
+    if (Caracter == "u" || Caracter == "U") {
+        basic.showString("ES VOCAL")
+    }
+})
